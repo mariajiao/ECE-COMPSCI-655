256d6956bf14373e4efcab69956aa177e1d76843
Maria Jiao <yj229@duke.edu>
2026-08-25 17:37:49 -0400

    lab0 done

--- lab0_task2/main/main.c
+++ lab0_task2/main/main.c
@@ -2,7 +2,62 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <stdio.h>
+#define LED_PIN 38
 
 void app_main(void) {
   // Write your code her
+    gpio_config_t io_conf = {
+         .pin_bit_mask = (1ULL << LED_PIN),
+         .mode = GPIO_MODE_OUTPUT,
+    };
+     gpio_config(&io_conf);
+
+     while (1) {
+        //S
+         gpio_set_level(LED_PIN, 1);
+         vTaskDelay(pdMS_TO_TICKS(500));
+         gpio_set_level(LED_PIN, 0);
+         vTaskDelay(pdMS_TO_TICKS(500));
+         gpio_set_level(LED_PIN, 1);
+         vTaskDelay(pdMS_TO_TICKS(500));
+         gpio_set_level(LED_PIN, 0);
+         vTaskDelay(pdMS_TO_TICKS(500));
+         gpio_set_level(LED_PIN, 1);
+         vTaskDelay(pdMS_TO_TICKS(500));
+         gpio_set_level(LED_PIN, 0);
+         vTaskDelay(pdMS_TO_TICKS(500));
+
+        //O
+         gpio_set_level(LED_PIN, 1);
+         vTaskDelay(pdMS_TO_TICKS(1000));
+         gpio_set_level(LED_PIN, 0);
+         vTaskDelay(pdMS_TO_TICKS(1000));
+         gpio_set_level(LED_PIN, 1);
+         vTaskDelay(pdMS_TO_TICKS(1000));
+         gpio_set_level(LED_PIN, 0);
+         vTaskDelay(pdMS_TO_TICKS(1000));
+         gpio_set_level(LED_PIN, 1);
+         vTaskDelay(pdMS_TO_TICKS(1000));
+         gpio_set_level(LED_PIN, 0);
+         vTaskDelay(pdMS_TO_TICKS(1000));
+
+        //S
+        gpio_set_level(LED_PIN, 1);
+         vTaskDelay(pdMS_TO_TICKS(500));
+         gpio_set_level(LED_PIN, 0);
+         vTaskDelay(pdMS_TO_TICKS(500));
+         gpio_set_level(LED_PIN, 1);
+         vTaskDelay(pdMS_TO_TICKS(500));
+         gpio_set_level(LED_PIN, 0);
+         vTaskDelay(pdMS_TO_TICKS(500));
+         gpio_set_level(LED_PIN, 1);
+         vTaskDelay(pdMS_TO_TICKS(500));
+         gpio_set_level(LED_PIN, 0);
+         vTaskDelay(pdMS_TO_TICKS(500));
+         
+
+
+     }
+
+
 }
