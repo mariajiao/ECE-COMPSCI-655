688ed1ca46361b45652cc2fc082fb487ddef2bdf
Maria Jiao <yj229@duke.edu>
2026-08-25 17:44:01 -0400

    lab0 task 1

--- lab0_task1/main/main.c
+++ lab0_task1/main/main.c
@@ -2,7 +2,22 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <stdio.h>
+#define LED_PIN 38
 
-void app_main(void) {
-// Write your code here
-}
+ void app_main() {
+     // Configure the LED pin
+     gpio_config_t io_conf = {
+         .pin_bit_mask = (1ULL << LED_PIN),
+         .mode = GPIO_MODE_OUTPUT,
+     };
+     gpio_config(&io_conf);
+
+     while (1) {
+         gpio_set_level(LED_PIN, 1);
+         vTaskDelay(pdMS_TO_TICKS(500));
+         gpio_set_level(LED_PIN, 0);
+         vTaskDelay(pdMS_TO_TICKS(500));
+
+
+     }
+ }
